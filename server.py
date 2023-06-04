5f658ce364268127bd26e47879311c14de0f1c6d
cevataykans <aykansevinc66@gmail.com>
2023-06-04 18:15:37 +0200

    Scale without sensitivity options

--- server.py
+++ server.py
@@ -2,21 +2,27 @@ import sys
 import time
 import threading
 import json
+import os
+import asyncio
+import math
 
 from scale import Scale
 from typing import Union
-from fastapi import FastAPI, Request, WebSocketDisconnect
+from fastapi import FastAPI, Request, WebSocketDisconnect, Response
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="./static"), name="static")
+
+folder = os.path.dirname(__file__)
+app.mount("/static", StaticFiles(directory= (folder + "/static").strip() ), name="static")
 templates = Jinja2Templates(directory="templates")
 
 origins = [
     "http://localhost",
-    "http://192.168.0.17"
+    "http://192.168.0.17",
+    "http://127.0.0.1"
 ]
 
 from fastapi.middleware.cors import CORSMiddleware
@@ -32,12 +38,15 @@ scale = Scale()
 cur_weight = 0
 exit = False
 tare_lock = threading.Lock()
+
 def scale_thread():
     global cur_weight
     global exit
     while not exit:
         tare_lock.acquire()
-        cur_weight = round(scale.weight())
+        cur_weight = round(scale.weight(), 2)
+        if cur_weight < 0:
+            cur_weight = 0
         tare_lock.release()
         time.sleep(0.1)
 
@@ -55,23 +64,23 @@ def shutdown_event():
 async def read_item(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.get("/api/v1/scale", status_code=200)
-async def tare_again():
+@app.post("/api/v1/tare")
+async def tare():
     print("Tare...")
     tare_lock.acquire()
     scale.tare()
     tare_lock.release()
-    return "Tare Success"
+    return Response(content="Tare Success", media_type="application/text")
 
 from fastapi import WebSocket
 @app.websocket("/api/v1/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     try:
-        while True:
+        while not exit:
             await websocket.send_text(json.dumps({
                 "value": cur_weight
             }))
-            time.sleep(0.2)
+            await asyncio.sleep(0.2)
     except WebSocketDisconnect:
         print("Client disconnected")
